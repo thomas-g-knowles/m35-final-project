5388a884e8716fbd1d58cc38b7c904cbf0046fe4
thomas-g-knowles <thomas.g.knowles@gmail.com>
2022-06-06 15:41:47 +0100

    Class refined and local QR code creation implemented.

--- src/android_enrollment.py
+++ src/android_enrollment.py
@@ -4,12 +4,13 @@ from googleapiclient.discovery import build
 import json
 from urllib.parse import urlencode
 import webbrowser
+import qrcode
 
-class Enroller:
-  '''Creates an object from class QRCode, used for provisioning and enrolling an android device. QRCode object has all methods needed to create an enrollment token.'''
+class deviceEnroller:
+  '''Creates an object from class deviceEnroller, used for provisioning and enrolling an android device. deviceEnroller object has all methods needed to create an enrollment token.'''
 
-  def __init__(self: object, paths: dict, debug: bool = True):
-    self.paths: dict = paths
+  def __init__(self: object, auth_path: str, debug: bool = False):
+    self.auth_path: str = auth_path
     self.debug = debug
     self.authenticate()
     self.api_client()
@@ -18,8 +19,9 @@ class Enroller:
   def authenticate(self: object):
     '''Authenticates the service account associated with the JSON file argument passed.'''
 
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = self.paths["auth"]
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = self.auth_path
     google.auth.default()
+    del self.auth_path
 
     if self.debug:
       print("- Authentication succeeded")
@@ -34,57 +36,19 @@ class Enroller:
       print("- API client built successfully")
 
 
-  def patch_policy(self: object, policy_name: str, policy_json: dict):
+  def patch_policy(self: object, policy_path: str, policy_name: str, policy_json: dict):
     '''Patches/updates existing policy with passed policy JSON OR creates a new one if no existing policy is found for given name.'''
 
-    self.androidmanagement.enterprises().policies().patch(name=self.paths["policy"]+policy_name, body=policy_json).execute()
+    self.androidmanagement.enterprises().policies().patch(name=policy_path+policy_name, body=policy_json).execute()
 
     if self.debug:
       print("- Policy patched successfully")
 
 
-  def list_policies(self: object):
-    '''Lists all existing policies under an enterprise.'''
-
-    try:
-      return self.androidmanagement.enterprises().policies().list(parent=self.paths["enterprise"]).execute()["policies"]
-
-    except KeyError:
-      if self.debug:
-        print("- No policies exist under enterprise")
-        return False
-
-
-  def delete_policies(self: object):
-    '''Deletes all enrolled devices under enterprise.'''
-
-    if self.list_policies():
-
-      if self.debug:
-        deleted_policies = []
-
-      for policy in self.list_policies():
-        policy_name = policy["name"]
-        print(self.androidmanagement.enterprises().policies().delete(name=policy_name).execute())
-        if self.debug:
-          deleted_policies.append(policy_name)
-      
-      if self.debug:
-        print("- The following policies have been deleted from the enterprise:", deleted_policies)
-      
-    else:
-      print("- No policies exist under enterprise to delete")
-
-
-  def policy_tokenization(self: object, policy_choice: int):
+  def policy_tokenization(self: object, enterprise_path: str, policy_path: str, policy_choice: str):
     '''Generates an enrollment token from the discovery API. Note that it indexes and returns only the QR code portion (only required section).'''
 
-    if policy_choice != 5:
-      self.enrollment_token: dict = self.androidmanagement.enterprises().enrollmentTokens().create(parent=self.paths["enterprise"], body={"policyName": self.paths["policy"]+"policy"+str(policy_choice)}).execute()["qrCode"]
-
-    else:
-      self.enrollment_token: dict = self.androidmanagement.enterprises().enrollmentTokens().create(parent=self.paths["enterprise"], body={"policyName": self.paths["policy"]+"devmode"}).execute()["qrCode"]
-
+    self.enrollment_token: dict = self.androidmanagement.enterprises().enrollmentTokens().create(parent=enterprise_path, body={"policyName": policy_path+"policy"+policy_choice}).execute()["qrCode"]
 
     if self.debug:
       print(f"- Enrollment token generated: \n{self.enrollment_token}")
@@ -123,67 +87,13 @@ class Enroller:
       print("- QR code opened in web browser")
 
 
-  def get_url(self: object):
-    '''Returns URL of QR code when invoked, to make further manipulation possible rather than just printing it out.'''
-    
-    return self.qrcode_url
+  def generate_qrcode(self: object):
+    '''Generates QR code PNG image file, in project root directory.'''
 
-
-  def list_devices(self: object):
-    '''Returns list of dicts containing all enrolled devices info under enterprise.'''
-
-    try:
-      return self.androidmanagement.enterprises().devices().list(parent=self.paths["enterprise"]).execute()["devices"]
-    except KeyError:
-      if self.debug:
-        print("- No devices enrolled under enterprise")
-      return False
-
-
-  def list_device(self: object, device_id: str):
-    '''Returns enrolled device info under enterprise.'''
-
-    return self.androidmanagement.enterprises().devices().get(name=device_id).execute()
-
-
-  def delete_devices(self: object):
-    '''Deletes all enrolled devices under enterprise.'''
+    qr = qrcode.QRCode(version=1, box_size=10, border=5)
+    qr.add_data(self.enrollment_token)
+    qr.make(fit=True)
+    qr.make_image(fill="black").save("./qrcode.png")
 
     if self.debug:
-      deleted_devices = []
-
-    if self.list_devices():
-      for device in self.list_devices():
-        device_id = device["name"][device["name"].rfind("/")+1:]
-        print(self.androidmanagement.enterprises().devices().delete(name=device["name"]).execute())
-        if self.debug:
-          deleted_devices.append(device_id)
-      
-      if self.debug:
-        print("- The following devices have been deleted from the enterprise:", deleted_devices)
-
-    else:
-      if self.debug:
-        print("- No devices exist under enterprise to delete")
-
-
-  def delete_device(self: object, device_id: str):
-    '''Deletes specified enrolled device under enterprise, doubles as a remote wipe capability - does not enforce FRP.'''
-
-    self.androidmanagement.enterprises().devices().delete(name=self.paths["enterprise"]+"/devices/"+device_id).execute()
-    
-    if self.debug:
-      print("- Device with ID:", device_id[device_id.rfind("/"):], "has been deleted from the enterprise")
-
-
-  def device_policy(self: object, device_id: str, policy_name: str):
-    '''Sets the policy for an individual device by targetting its device ID.'''
-    if policy_name != "5":
-      self.androidmanagement.enterprises().devices().patch(name=self.paths["enterprise"]+"/devices/"+device_id, updateMask="policyName", body={"policyName": self.paths["policy"]+"policy"+policy_name}).execute()
-    
-    else:
-      self.androidmanagement.enterprises().devices().patch(name=self.paths["enterprise"]+"/devices/"+device_id, updateMask="policyName", body={"policyName": self.paths["policy"]+"devmode"}).execute()
-
-    if self.debug:
-      print("- Device with ID:", device_id, "has been updated to policy:", policy_name)
-
+      print("Local QR code PNG generated.")
--- src/main.py
+++ src/main.py
@@ -3,27 +3,35 @@ from dotenv import load_dotenv
 # Imports OS to assist management of .env file:
 import os
 # Imports self made class to create enrollment token:
-from android_enrollment import Enroller
+from android_enrollment import deviceEnroller
 
 if __name__ == "__main__":
   print("\n------------ QR Code Enrollment Generator Started ------------\n")
 
   # Environment variables:
   load_dotenv("./.env")
-  paths = {"enterprise": os.environ.get("ENTERPRISE_PATH"), "policy": os.environ.get("POLICY_PATH"), "auth": os.environ.get("AUTH_PATH")}
-  wifi_info = {"ssid": os.environ.get("WIFI_SSID"), "password": os.environ.get("WIFI_PASSWORD"), "security": os.environ.get("WIFI_SECURITY")}
+  auth_path: str = os.environ.get("AUTH_PATH")
+  enterprise_path: str = os.environ.get("ENTERPRISE_PATH")
+  policy_path: str = os.environ.get("POLICY_PATH")
+  wifi_info: dict = {"ssid": os.environ.get("WIFI_SSID"), "password": os.environ.get("WIFI_PASSWORD"), "security": os.environ.get("WIFI_SECURITY")}
 
   # Instantiating QRCode object:
-  my_qrcode = Enroller(paths)
+  enroller_instance = deviceEnroller(auth_path, debug=True)
+
+  # Creates the specified policy if it cannot be found:
+  enroller_instance.patch_policy(policy_path, "temp", {"applications": [{"installType": "FORCE_INSTALLED", "packageName": "com.sega.sonic"}, {"installType": "FORCE_INSTALLED", "packageName": "com.sega.sonic1"}, {"installType": "FORCE_INSTALLED", "packageName": "com.sega.sonic1px"}, {"installType": "FORCE_INSTALLED", "packageName": "com.yodo1.crossyroad"}]})
 
   # Generates the enrollment token:
-  my_qrcode.policy_tokenization("0")
+  enroller_instance.policy_tokenization(enterprise_path, policy_path, "temp")
 
   # QR code config manipulation:
-  my_qrcode.token_injection(wifi_info)
+  enroller_instance.token_injection(wifi_info)
 
   # Generates URL for QR code:
-  my_qrcode.generate_url()
+  enroller_instance.generate_url()
 
   # Displays QR code in browser automatically:
-  my_qrcode.open_url()
+  enroller_instance.open_url()
+
+  # Generation of QR code PNG:
+  enroller_instance.generate_qrcode()
